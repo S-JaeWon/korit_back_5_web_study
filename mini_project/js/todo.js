adef2258fe77f782d0d8a9df713c3d4a7f50f34f
S-JaeWon <ssm5170@gmail.com>
2024-02-19 18:24:22 +0900

    feat: mini P, 추가, 삭제 버튼 기능

--- mini_project/js/todo.js
+++ mini_project/js/todo.js
@@ -1,11 +1,26 @@
-function getTodoList() {
+function handleAddClick() {
     const contentList = document.querySelector(".content-list")
+    const contentText = document.querySelector(".content-text")
+    
+    contentList.innerHTML += `
+        <li class="content-inputs">
+            ${contentText.value} 
+            <input type="checkbox" class="content-checkbox">
+        </li>
+    `;
 
-    contentList.innerHTML = "";
+    contentText.value = "";
+}
 
-    for(let content of contentList) {
-        contentList.innerHTML += `
-        <li class="content-inputs">${}<input type="checkbox" class="content-checkbox"></li>
-        `;
-    }
+function handleRemoveClick() {
+    const contentList = document.querySelector(".content-list");
+    const checkboxes = document.querySelectorAll(".content-checkbox");
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            // 체크된 경우 해당 li 요소를 찾아서 삭제합니다.
+            const listItem = checkbox.closest(".content-inputs");
+            contentList.removeChild(listItem);
+        }
+    });
 }
